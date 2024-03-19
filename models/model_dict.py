287ef67b992e1814edc614e1edd88b316b89ec4c
usagisukisuki <93986825+usagisukisuki@users.noreply.github.com>
2024-03-19 11:52:53 +0900

    Update model_dict.py

--- models/model_dict.py
+++ models/model_dict.py
@@ -5,8 +5,6 @@ from models.segment_anything_lora.build_sam import sam_lora_model_registry
 from models.segment_anything_convlora.build_sam import sam_convlora_model_registry
 from models.segment_anything_adaptformer.build_sam import sam_adaptformer_model_registry
 from models.segment_anything_samus.build_sam_us import samus_model_registry
-#from models.segment_anything_SMadaptformer.build_sam import sam_smadaptformer_model_registry
-#from models.generalized_segment_anything.build_sam import gsam_model_registry
 
 def get_model(modelname="SAM", args=None):
     if modelname == "SAM":
@@ -23,10 +21,6 @@ def get_model(modelname="SAM", args=None):
         model = sam_adaptformer_model_registry['vit_b'](args=args, checkpoint=args.sam_ckpt)
     elif modelname == "SAMUS":
         model = samus_model_registry['vit_b'](args=args, checkpoint=args.sam_ckpt)
-    #elif modelname == "SAM_SMAdaptFormer":
-    #    model = sam_smadaptformer_model_registry['vit_b'](args=args,checkpoint=args.sam_ckpt)
-    #elif modelname == "GSAM":
-    #    model = gsam_model_registry['vit_b'](args=args, checkpoint=args.sam_ckpt)
     else:
         raise RuntimeError("Could not find the model:", modelname)
     return model
